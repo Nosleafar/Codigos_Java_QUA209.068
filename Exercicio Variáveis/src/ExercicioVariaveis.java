7f21886755bd4fd339072d55d274bfd23bc05855
Nosleafar <rafaelsonsilva.rs@gmail.com>
2023-08-03 16:49:19 -0300

    Correção de Exercicios 1 e 2, e exemplo de entrada de dados

--- Exercicio Variáveis/src/ExercicioVariaveis.java
+++ Exercicio Variáveis/src/ExercicioVariaveis.java
@@ -2,8 +2,21 @@
 public class ExercicioVariaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int numero1 = 5;
+		int numero2 = 10;
+		boolean verdadeiro = true;
+		boolean falso = false;
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		System.out.println("verdadeiro");
+		
+		String nome = "Rafaelson";
+		String nome2 = "da Silva Souza";
+		
+		System.out.print (nome);
+		System.out.print (nome2);
 	}
 
 }
