7399765a5c7d5374989b6c7b3514c3175fc7797d
Nosleafar <rafaelsonsilva.rs@gmail.com>
2023-08-07 17:49:30 -0300

    atualização

---
+++ Exercícios_07_08_2023/src/exercicio4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exercicio4 {
+
+	public static void main(String[] args) {
+	    	Scanner leia = new Scanner(System.in);
+	    	
+	    	System.out.println("Digite um número");
+            int A;
+			A = leia.nextInt();
+			System.out.println("Digite um número");
+            int B;
+			B = leia.nextInt();
+			System.out.println("Digite um número");
+            int C;
+			C = leia.nextInt();
+     
+			 }
+			
+	}
+
+}
