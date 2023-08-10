29271a0c03cfea7b1e7ce070c514d4afcf32d4db
Nosleafar <rafaelsonsilva.rs@gmail.com>
2023-08-10 15:09:33 -0300

    atualização

--- ExerciciodeRepetição/src/principal/exercicio4.java
+++ ExerciciodeRepetição/src/principal/exercicio4.java
@@ -11,17 +11,8 @@ public class exercicio4 {
 		int num1 = 0;
 		int num2 = 0;
 		
-		System.out.print("Entre com o primeiro numero: ");
-		num1 = sc.nextInt();
+
 		
-		System.out.println("Entre com o segundo numero: ");
-		num2 = sc.nextInt();
-		
-		for(int contador = ++num1; contador < num2; contador++) {
-		System.out.println(contador);
-		}
-		
-		sc.close();
 	}
 
 }
