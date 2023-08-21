fffaf8e20492cf95c9826b146e5bc2b66cb606bf
Nosleafar <rafaelsonsilva.rs@gmail.com>
2023-08-21 17:29:26 -0300

    Mais exercícios

---
+++ ExerciciosPPO/src/principal/Exercicio3.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		//crie um programa que receba o nome, o e-mail e a senha do usuário, o usuário só poderá alterar
+		//a senha através de um método e a senha não pode ser mostrada na tela principal do programa;
+
+	}
+
+}
