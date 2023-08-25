906d06213a539a98b2a6cfbca3e40e2a375659bc
Nosleafar <rafaelsonsilva.rs@gmail.com>
2023-08-25 17:24:24 -0300

    revisão

--- ExerciciosListas/src/principal/Produto.java
+++ ExerciciosListas/src/principal/Produto.java
@@ -2,45 +2,38 @@ package principal;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
-import entities.especificações;
-import entities.pessoal;
+import entities.produto;
 
-public class Produto {
-
-    public static void main(String[] args) {
-
-        ArrayList<String> dados = new ArrayList<>();
-          Scanner sc = new Scanner(System.in);
-   {
-            System.out.println("Digite o nome do Produto: ");
-            String nome = sc.nextLine();
-            System.out.println("Digite a quantidade do Produto: ");
-            double quantidade = sc.nextDouble();
-            System.out.println("Digite o valor: ");
-            int valor = sc.nextInt();
-            System.out.println("Digite o nome do Produto: ");
-            String nome2 = sc.nextLine();
-            System.out.println("Digite a quantidade do Produto: ");
-            double quantidade2 = sc.nextDouble();
-            System.out.println("Digite o valor: ");
-            int valor2 = sc.nextInt();
-            System.out.println("Digite o nome do Produto: ");
-            String nome3 = sc.nextLine();
-            System.out.println("Digite a quantidade do Produto: ");
-            double quantidade3 = sc.nextDouble();
-            System.out.println("Digite o valor: ");
-            int valor3 = sc.nextInt();
-            
-            
-        }
-
-        Collections.sort(dados);
-
-        
-		for (especificações produto::especificações) {
-            System.out.println(dados);
-        }
-    }
+public abstract class Produto implements Comparable <Produto>{
+	public static void main (String [] args) {
+	
+		
+		produto produto;
+		Scanner sc = new Scanner (System.in);
+		ArrayList<produto> produtos = new ArrayList<>();
+		
+		
+		for (int i = 1; i <4; i++) {
+		produto = new produto();
+		
+		System.out.println("Digite o produto: ");
+		produto.setNome(sc.nextLine());
+		
+		System.out.println("Digite a quantidade: ");
+		produto.setQuantidade(sc.nextInt());
+		
+		System.out.println("Digite o preço: ");
+		produto.setPreço(sc.nextDouble());
+		sc.nextLine();
+		
+		produtos.add(produto);
+		}
+		
+	
+		System.out.println(produtos);
+	}
 }
